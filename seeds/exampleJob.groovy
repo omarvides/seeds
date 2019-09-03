340a726e91452bffeeae2300e084441aaf9794b4
Walter Vides <omarvides@gmail.com>
2019-09-03 23:16:16 +0200

    Adding pattern

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -1,9 +1,15 @@
-mulitbranchPipelineJob('example') {
-  branchSources {
-    git {
-      remote('git@github.com:omarvides/dockerized-api.git')
-      credentialsId('github-user')
-      includes('*')
+multibranchPipelineJob('example') {
+    branchSources {
+        git {
+            id('123456789') // IMPORTANT: use a constant and unique identifier
+            remote('https://github.com/jenkinsci/job-dsl-plugin.git')
+            credentialsId('github-ci')
+            includes('JENKINS-*')
+        }
     }
-  }
+    orphanedItemStrategy {
+        discardOldItems {
+            numToKeep(20)
+        }
+    }
 }
