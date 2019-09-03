f66cd4cacb846d0fa0d03a00d38a213c6a5dfc39
Walter Vides <omarvides@gmail.com>
2019-09-03 23:19:02 +0200

    Adding pattern

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -1,6 +1,7 @@
 multibranchPipelineJob('example') {
   branchSources {
     git {
+      id('84aff50e-2652-4e95-a102-5eccc1c62e95')
       remote('git@github.com:omarvides/dockerized-api.git')
       credentialsId('github-user')
       includes('*')
