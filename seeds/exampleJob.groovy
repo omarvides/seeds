3f1e5c92660d1a6350dcb318044f3e262f69430c
Walter Vides <omarvides@gmail.com>
2019-09-07 17:05:42 +0200

    Adding github trigger

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -1,6 +1,7 @@
 String repo = 'omarvides/dockerized-api'
 
 multibranchPipelineJob('dockerized-api') {
+  configure { it / 'triggers' / 'com.cloudbees.jenkins.GitHubPushTrigger' / 'spec' }
   branchSources {
     github {
       id('84aff50e-2652-4e95-a102-5eccc1c62e95')
