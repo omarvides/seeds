a5df00f619692f5ad5d18cc8fe60269f5d6707ff
Walter Vides <omarvides@gmail.com>
2019-09-07 11:58:00 +0200

    Changing scm strategy

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -1,10 +1,9 @@
+String repo = 'omarvides/dockerized-api'
+
 multibranchPipelineJob('example') {
   branchSources {
-    github {
-      id('84aff50e-2652-4e95-a102-5eccc1c62e95')
-      repository('omarvides/dockerized-api')
-      checkoutCredentialsId('github-user')
-      includes('*')
+    scm {
+      github repo
     }
   }
 }
