ba44bdf2ef3d792f0faa35c17b113b9d0e03ae7b
Walter Vides <omarvides@gmail.com>
2019-09-07 12:54:33 +0200

    Updating multibranchpipeline strategy

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -4,7 +4,8 @@ multibranchPipelineJob('example') {
   branchSources {
     github {
       id('84aff50e-2652-4e95-a102-5eccc1c62e95')
-      repository('omarvides/dockerized-api')
+      repository('dockerized-api')
+      repoOwner('omarvides')
       checkoutCredentialsId('github-user')
       includes('*')
     }
