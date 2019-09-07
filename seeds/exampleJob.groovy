9539d475bb455d3ae0b2717994df081abcea49c3
Walter Vides <omarvides@gmail.com>
2019-09-07 11:56:45 +0200

    Removing .git from exampleJob.groovy

--- seeds/exampleJob.groovy
+++ seeds/exampleJob.groovy
@@ -2,7 +2,7 @@ multibranchPipelineJob('example') {
   branchSources {
     github {
       id('84aff50e-2652-4e95-a102-5eccc1c62e95')
-      repository('omarvides/dockerized-api.git')
+      repository('omarvides/dockerized-api')
       checkoutCredentialsId('github-user')
       includes('*')
     }
